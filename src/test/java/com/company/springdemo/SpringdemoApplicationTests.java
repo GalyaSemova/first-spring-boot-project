5b5431d8288a1f4ec2f21251b85b474af801e5a5
fondle <semovagalina@gmail.com>
2023-04-26 15:21:14 +0300

    initializing spring boot project

---
+++ src/test/java/com/company/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.company.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+
+
+	void contextLoads() {
+	}
+
+}
