9bf7bfa35cb74fef655dad3ecd7fa62d8181e86c
fondle <semovagalina@gmail.com>
2023-05-02 15:09:28 +0300

    implementing dao layer

--- src/main/java/com/company/springdemo/student/StudentService.java
+++ src/main/java/com/company/springdemo/student/StudentService.java
@@ -5,25 +5,18 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 
-@Service
-public class StudentService {
-    public List<Student> findAllStudents() {
-        return List.of(
-                new Student(
-                        "Galya",
-                        "Semova",
-                        LocalDate.now(),
-                        "test@gmail.com",
-                        35
-                ),
-                new Student(
-                        "Galina",
-                        "Student 2",
-                        LocalDate.now(),
-                        "test2@gmail.com",
-                        24
-                )
 
-        );
-    }
+public interface StudentService {
+
+    Student save(Student s);
+
+    List<Student> findAllStudents();
+
+    Student findByEmail(String email);
+
+    Student update(Student s);
+
+    void delete(String email);
+
+
 }
